47d07c6cc9e4a7c582fa33f4afd623d2f65f9fed
weijiex <weijiex@uchicago.edu>
2017-11-20 14:07:57 -0600

    sample_try_line_chart

---
+++ old_try/script.js
@@ -0,0 +1,254 @@
+//setup spacing size
+var svg_w = 960;
+var svg_h = 700;
+var margin = {top: 90, right: 15, bottom: 180, left: 110};
+var width = svg_w - margin.left - margin.right;
+var height = svg_h - margin.top - margin.bottom;
+
+var formatYear = d3.timeFormat("%Y");
+var parseYear = d3.timeParse("%Y");
+
+// Set the ranges
+var x = d3.scaleTime().domain([parseYear("2008"), parseYear("2013")]).range([0, width]);
+var y = d3.scaleLinear().range([height, 0]);
+
+
+// Define the line
+var valueLine = d3.line()
+    .x(function(d) { return x(+d.year); })
+    .y(function(d) { return y(+d.number); })
+
+
+
+// Create the svg canvas in the "graph" div
+var svg = d3.select("#graph")
+        .append("svg")
+        .style("width", svg_w + "px")
+        .style("height", svg_h + "px")
+        .attr("width", svg_w)
+        .attr("height", svg_h)
+        .append("g")
+        .attr("transform","translate(" + margin.left + "," + margin.top + ")")
+        .attr("class", "svg");
+
+//Import the CSV data
+d3.json("data.json", function(error, data) {
+	if (error) {console.log(error);}
+
+	data.forEach(function(d) {
+		d.number = +d.number; 
+		d.year = +d.year;
+		d.geo = d.geo;
+		d.topic = d.topic;
+		d.geo_entity_name = d.geo_entity_name
+	});
+
+	
+
+	var nest = d3.nest()
+	  .key(function(d){
+	    return d.topic;
+	  })
+	  .entries(data)
+
+	  console.log(nest.values)
+	  console.log(data)
+	  //console.log(valueLine(nest))
+
+  	// Scale the range of the data	
+	var years = data.map(function(d) {return d.year;});
+	var yearScale = d3.scaleBand()
+        .domain(years)
+        .range([0, width]);
+
+    //var bandwidth = yearScale.bandwidth();
+        
+
+	//y.domain([0, d3.max(data, function(d) { return d.number; })]);
+	y.domain([0, d3.max(data, function(d) { return d.number; })]);  
+	// Set up the x axis
+	var xaxis = svg.append("g")
+	       .attr("transform", "translate(0," + height + ")")
+	       .attr("class", "x axis")
+	       .call(d3.axisBottom(yearScale)
+	          .tickSize(0, 0)
+	          .tickSizeInner(0)
+	          .tickPadding(10));
+
+	 var yaxis = svg.append("g")
+		       .attr("class", "y axis")
+		       .call(d3.axisLeft(y)
+		          .ticks(5)
+		          .tickSizeInner(0)
+		          .tickPadding(6)
+		          .tickSize(0, 0));
+
+
+	  // Add a label to the y axis
+	  svg.append("text")
+	        .attr("transform", "rotate(-90)")
+	        .attr("y", 0 - 60)
+	        .attr("x", 0 - (height / 2))
+	        .attr("dy", "1em")
+	        .style("text-anchor", "middle")
+	        .text("Rate")
+	        .attr("class", "y axis label");
+
+
+	        svg.selectAll(".line")
+      .data(nest)
+      .enter()
+      .append("path")
+	      .attr("class", "line")
+	      .attr("d", function(d){
+	      	return valueLine(d.values)});
+  
+
+/*
+	// Create 1st dropdown
+    var topicMenu = d3.select("#topicDropdown")
+
+    topicMenu
+		.append("select")
+		.selectAll("option")
+        .data(nest)
+        .enter()
+        .append("option")
+        .attr("value", function(d){
+            return d.key;
+        })
+        .text(function(d){
+            return d.key;
+        })
+    // Create 2nd dropdown
+    var entityMenu = d3.select("#entityDropdown")
+
+    entityMenu
+    	.data(nest)
+		.append("select")
+		.selectAll("option")
+        .data(function(d) { return d.value.entity; })
+       	.enter()
+        .append("option")
+        .attr("value", function(d){
+            return d.key;
+        })
+        .text(function(d){
+            return d.key;
+        })
+
+
+
+    // Function to create the initial graph
+ 	var initialGraph = function(topic){
+
+ 		// Filter the data to include only fruit of interest
+ 		var selectTopic = nest.filter(function(d){
+                return d.key == topic;
+              })
+
+	    var selectTopicGroups = svg.selectAll(".topicGroups")
+		    .data(selectTopic, function(d){
+		      return d ? d.key : this.key;
+		    })
+		    .enter()
+		    .append("g")
+		    .attr("class", "topicGroups")
+		    
+
+		var initialPath = selectTopicGroups.selectAll(".line")
+			.data(function(d) { return d.values; })
+			.enter()
+			.append("path")
+
+
+
+		initialPath
+			.attr("d", function(d){
+				return valueLine(d.values)
+			})
+			.attr("class", "line")
+ 	}
+
+ 	// Create initial graph
+ 	initialGraph("Cigarette Smoking among Adults") 
+/*
+ 	// Update the data
+ 	var updateGraph = function(topic){
+
+ 		// Filter the data to include only topic of interest
+ 		var selectTopic = nest.filter(function(d){
+                return d.key == topic;
+              })
+
+ 		// Select all of the grouped elements and update the data
+	    var selectTopicGroups = svg.selectAll(".topicGroups")
+		    .data(selectTopic)
+		    .each(function(d){
+                y.domain([0, d.value.max])
+            });
+
+		    // Select all the lines and transition to new positions
+            selectTopicGroups.selectAll("path.line")
+               .data(function(d) { return d.value.entity; }, 
+               		function(d){ return d.key; })
+               .transition()
+                  .duration(1000)
+                  .attr("d", function(d){
+                    return valueLine(d.values)
+                  })
+
+        // Update the Y-axis
+            d3.select(".y")
+                    .transition()
+                    .duration(1500)
+                    .call(d3.axisLeft(y)
+                      .ticks(5)
+                      .tickSizeInner(0)
+                      .tickPadding(6)
+                      .tickSize(0, 0));
+
+
+ 	}
+
+ 	// Run update function when dropdown selection changes
+ 	topicMenu.on('change', function(){
+
+ 		// Find which topic was selected from the dropdown
+ 		var selectedTopic = d3.select(this)
+            .select("select")
+            .property("value")
+
+        // Run update function with the selected topic
+        updateGraph(selectedTopic)
+
+
+    });
+
+
+    // Change color of selected line when year dropdown changes
+    entityMenu.on('change', function(){
+
+    	// Find which year was selected
+    	var selectedEntity = d3.select(this)
+    		.select("select")
+    		.property("value")
+
+    	// Change the class of the matching line to "selected"
+    	var selLine = svg.selectAll(".line")
+              // de-select all the lines
+              .classed("selected", false)
+              .filter(function(d) {
+                  return +d.key === +selectedEntity
+              })
+              // Set class to selected for matching line
+              .classed("selected", true)
+              .raise()
+    })
+
+*/
+
+	console.log(data);
+});
+
+
--- script.js
+++ script.js
@@ -1,253 +1,57 @@
-//setup spacing size
-var svg_w = 960;
-var svg_h = 700;
-var margin = {top: 90, right: 15, bottom: 180, left: 110};
-var width = svg_w - margin.left - margin.right;
-var height = svg_h - margin.top - margin.bottom;
+// set the dimensions and margins of the graph
+var margin = {top: 20, right: 20, bottom: 30, left: 50},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
 
-var formatYear = d3.timeFormat("%Y");
-var parseYear = d3.timeParse("%Y");
 
-// Set the ranges
-var x = d3.scaleTime().domain([parseYear("2008"), parseYear("2013")]).range([0, width]);
+// set the ranges
+var x = d3.scalePoint().range([0, width]);
 var y = d3.scaleLinear().range([height, 0]);
 
+// define the line
+var valueline = d3.line()
+    .x(function(d) { return x(d.year); })
+    .y(function(d) { return y(+d.number); });
 
-// Define the line
-var valueLine = d3.line()
-    .x(function(d) { return x(+d.year); })
-    .y(function(d) { return y(+d.number); })
+// append the svg obgect to the body of the page
+// appends a 'group' element to 'svg'
+// moves the 'group' element to the top left margin
+var svg = d3.select("body").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
 
+// Get the data
+d3.csv("sample_1.csv", function(error, data) {
+  if (error) throw error;
 
+  // format the data
+  data.forEach(function(d) {
+      d.year = d.year;
+      d.number = +d.number;
+  });
 
-// Create the svg canvas in the "graph" div
-var svg = d3.select("#graph")
-        .append("svg")
-        .style("width", svg_w + "px")
-        .style("height", svg_h + "px")
-        .attr("width", svg_w)
-        .attr("height", svg_h)
-        .append("g")
-        .attr("transform","translate(" + margin.left + "," + margin.top + ")")
-        .attr("class", "svg");
+   // Scale the range of the data
+  x.domain([2008, 2009, 2010, 2011, 2012, 2013]);
+  y.domain([0, d3.max(data, function(d) { return d.number; })]);
 
-//Import the CSV data
-d3.json("data.json", function(error, data) {
-	if (error) {console.log(error);}
+  // Add the valueline path.
+  svg.append("path")
+      .data([data])
+      .attr("class", "line")
+      .attr("d", valueline);
 
-	data.forEach(function(d) {
-		d.number = +d.number; 
-		d.year = +d.year;
-		d.geo = d.geo;
-		d.topic = d.topic;
-		d.geo_entity_name = d.geo_entity_name
-	});
+  // Add the X Axis
+  svg.append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x))
 
-	
+      
 
-	var nest = d3.nest()
-	    .key(function(d){
-	    	return d.topic;
-	    })
-		.rollup(function(leaves){
-            var max = d3.max(leaves, function(d){
-            	return d.number
-            })
-            var entity = d3.nest().key(function(d){
-            	return d.geo_entity_name
-            })
-            .entries(leaves);
-            return {max:max, entity:entity};
-            })
-	  .entries(data)
-	  console.log(nest)
-	  console.log(valueLine(nest))
+  // Add the Y Axis
+  svg.append("g")
+      .call(d3.axisLeft(y));
 
-  	// Scale the range of the data	
-	var years = data.map(function(d) {return d.year;});
-	var yearScale = d3.scaleBand()
-        .domain(years)
-        .range([0, width]);
-
-    //var bandwidth = yearScale.bandwidth();
-        
-
-	//y.domain([0, d3.max(data, function(d) { return d.number; })]);
-	  
-	// Set up the x axis
-	var xaxis = svg.append("g")
-	       .attr("transform", "translate(0," + height + ")")
-	       .attr("class", "x axis")
-	       .call(d3.axisBottom(yearScale)
-	          .tickSize(0, 0)
-	          .tickSizeInner(0)
-	          .tickPadding(10));
-
-	// Create 1st dropdown
-    var topicMenu = d3.select("#topicDropdown")
-
-    topicMenu
-		.append("select")
-		.selectAll("option")
-        .data(nest)
-        .enter()
-        .append("option")
-        .attr("value", function(d){
-            return d.key;
-        })
-        .text(function(d){
-            return d.key;
-        })
-
-    // Create 2nd dropdown
-    var entityMenu = d3.select("#entityDropdown")
-
-    entityMenu
-    	.data(nest)
-		.append("select")
-		.selectAll("option")
-        .data(function(d) { return d.value.entity; })
-       	.enter()
-        .append("option")
-        .attr("value", function(d){
-            return d.key;
-        })
-        .text(function(d){
-            return d.key;
-        })
-
-    // Function to create the initial graph
- 	var initialGraph = function(topic){
-
- 		// Filter the data to include only fruit of interest
- 		var selectTopic = nest.filter(function(d){
-                return d.key == topic;
-              })
-
-	    var selectTopicGroups = svg.selectAll(".topicGroups")
-		    .data(selectTopic, function(d){
-		      return d ? d.key : this.key;
-		    })
-		    .enter()
-		    .append("g")
-		    .attr("class", "topicGroups")
-		    .each(function(d){
-                y.domain([0, d.value.max])
-            });
-
-		var initialPath = selectTopicGroups.selectAll(".line")
-			.data(function(d) { return d.value.entity; })
-			.enter()
-			.append("path")
-
-
-
-		initialPath
-			.attr("d", function(d){
-				return valueLine(d.values)
-			})
-			.attr("class", "line")
-
-		  // Add the Y Axis
-		   var yaxis = svg.append("g")
-		       .attr("class", "y axis")
-		       .call(d3.axisLeft(y)
-		          .ticks(5)
-		          .tickSizeInner(0)
-		          .tickPadding(6)
-		          .tickSize(0, 0));
-		  
-		  // Add a label to the y axis
-		  svg.append("text")
-		        .attr("transform", "rotate(-90)")
-		        .attr("y", 0 - 60)
-		        .attr("x", 0 - (height / 2))
-		        .attr("dy", "1em")
-		        .style("text-anchor", "middle")
-		        .text("Rate")
-		        .attr("class", "y axis label");
-
- 	}
-
- 	// Create initial graph
- 	initialGraph("Cigarette Smoking among Adults")
-/*
- 	// Update the data
- 	var updateGraph = function(topic){
-
- 		// Filter the data to include only topic of interest
- 		var selectTopic = nest.filter(function(d){
-                return d.key == topic;
-              })
-
- 		// Select all of the grouped elements and update the data
-	    var selectTopicGroups = svg.selectAll(".topicGroups")
-		    .data(selectTopic)
-		    .each(function(d){
-                y.domain([0, d.value.max])
-            });
-
-		    // Select all the lines and transition to new positions
-            selectTopicGroups.selectAll("path.line")
-               .data(function(d) { return d.value.entity; }, 
-               		function(d){ return d.key; })
-               .transition()
-                  .duration(1000)
-                  .attr("d", function(d){
-                    return valueLine(d.values)
-                  })
-
-        // Update the Y-axis
-            d3.select(".y")
-                    .transition()
-                    .duration(1500)
-                    .call(d3.axisLeft(y)
-                      .ticks(5)
-                      .tickSizeInner(0)
-                      .tickPadding(6)
-                      .tickSize(0, 0));
-
-
- 	}
-
- 	// Run update function when dropdown selection changes
- 	topicMenu.on('change', function(){
-
- 		// Find which topic was selected from the dropdown
- 		var selectedTopic = d3.select(this)
-            .select("select")
-            .property("value")
-
-        // Run update function with the selected topic
-        updateGraph(selectedTopic)
-
-
-    });
-
-
-    // Change color of selected line when year dropdown changes
-    entityMenu.on('change', function(){
-
-    	// Find which year was selected
-    	var selectedEntity = d3.select(this)
-    		.select("select")
-    		.property("value")
-
-    	// Change the class of the matching line to "selected"
-    	var selLine = svg.selectAll(".line")
-              // de-select all the lines
-              .classed("selected", false)
-              .filter(function(d) {
-                  return +d.key === +selectedEntity
-              })
-              // Set class to selected for matching line
-              .classed("selected", true)
-              .raise()
-    })
-
-*/
-
-	console.log(data);
 });
-
-
